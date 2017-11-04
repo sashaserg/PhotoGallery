6aac4e461edcde05b0617b8a18b232881fea5b18
alexander <exirtt@gmail.com>
2017-11-04 20:03:33 +0200

    add form for adding photo

--- src/components/AddPhotoButton/AddPhotoButton.js
+++ src/components/AddPhotoButton/AddPhotoButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import {  Button, Glyphicon } from 'react-bootstrap'
+import {  FormGroup, FormControl, ControlLabel, Button, Glyphicon, Modal } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 class AddPhotoButton extends Component
@@ -9,14 +9,58 @@ class AddPhotoButton extends Component
     constructor(props)
     {
         super(props);
+
+        this.state = {
+            showModal: false
+        }
+    }
+
+    closeModalAddPhoto() {
+        this.setState({ showModal: false });
+
+    }
+
+    openModalAddPhoto() {
+        this.setState({ showModal: true });
     }
 
     render()
     {
         return(
+            <div>
+                <Button className={"btn-circle btn-lg"} id={"AddPhotoButton"} onClick = { (e) => { this.openModalAddPhoto(e) }   } ><Glyphicon glyph="camera" /></Button>
+                <Modal show={this.state.showModal} onHide = { (e) => { this.closeModalAddPhoto(e) }   }>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Новое фото</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
 
-            <Button className={"btn-circle btn-lg"} id={"AddPhotoButton"}><Glyphicon glyph="camera" /></Button>
+                        <form encType={"multipart/form-data"} method={"POST"}>
 
+                            <FormGroup controlId="file" bsSize="large">
+                                <ControlLabel>Фотография</ControlLabel>
+                                <FormControl
+                                    autoFocus
+                                    type="file"
+                                />
+                            </FormGroup>
+
+                        </form>
+
+                        <div>
+
+                            <img className={"newPhotoHolder"} src={"https://hakimziad.files.wordpress.com/2017/02/face-with-question-mark.jpg?w=1200"}/>
+
+                        </div>
+
+                        </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick = { (e) => { this.closeModalAddPhoto(e) }   } bsStyle={"success"}>Опубликовать</Button>
+                        <Button onClick = { (e) => { this.closeModalAddPhoto(e) }   } bsStyle={"danger"}>Отмена</Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>
         )
     }
 
